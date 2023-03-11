a54aed6044b26267f38386de52e40c38fed20420
Jonathan-itch2 <124762544+Jonathan-itch2@users.noreply.github.com>
2023-03-11 16:04:16 -0600

    Add files via upload

---
+++ EVA2_5_TIPOS_HERENCIA/src/main/java/com/mycompany/eva2_5_tipos_herencia/EVA2_5_TIPOS_HERENCIA.java
@@ -0,0 +1,21 @@
+package com.mycompany.eva2_5_tipos_herencia;
+
+public class EVA2_5_TIPOS_HERENCIA {
+
+    public static void main(String[] args) {
+        
+    }
+}
+
+class SuperClase{
+    
+}
+
+class OtraSuperClase{//NO SE PUEDE HEREDAR DE DOS 
+                     //O MAS SUPERCLASES    
+}
+
+                        //extends solo funciona para una superclase
+class BaseClase extends SuperClase{
+    
+}
