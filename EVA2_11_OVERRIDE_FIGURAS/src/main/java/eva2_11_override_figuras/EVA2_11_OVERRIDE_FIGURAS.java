25ea895dcc7ed227ee9b1fd35bdbbf4b4b2e8fae
Jonathan-itch2 <124762544+Jonathan-itch2@users.noreply.github.com>
2023-03-16 18:24:57 -0600

    Add files via upload

---
+++ EVA2_11_OVERRIDE_FIGURAS/src/main/java/eva2_11_override_figuras/EVA2_11_OVERRIDE_FIGURAS.java
@@ -0,0 +1,8 @@
+package eva2_11_override_figuras;
+
+public class EVA2_11_OVERRIDE_FIGURAS {
+
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ EVA2_11_OVERRIDE_FIGURAS/src/main/java/eva2_11_override_figuras/Figuras.java
@@ -0,0 +1,11 @@
+package eva2_11_override_figuras;
+
+public class Figuras {
+    public double calcularArea(){
+        return -1;
+    }
+    
+    public double calcularPeri(){
+        return -1;
+    }
+}
