9ee6dcb96bed788e31248d249d0dd82f77d95568
Jonathan-itch2 <124762544+Jonathan-itch2@users.noreply.github.com>
2023-03-16 18:25:14 -0600

    Add files via upload

---
+++ EVA2_12_CLASES_ABSTRACTAS/src/main/java/eva2_12_clases_abstractas/EVA2_12_CLASES_ABSTRACTAS.java
@@ -0,0 +1,18 @@
+package eva2_12_clases_abstractas;
+
+public class EVA2_12_CLASES_ABSTRACTAS {
+
+    public static void main(String[] args) {
+       Prueba prueba = new Prueba();
+       //NO SE PUEDE CREAR OBJETOS (INSTACIAS) DE CLASES ABSTRACTAS
+       PruebaAbstracta pAab = new PruebaAbstracta();
+    }
+}
+
+class Prueba{
+    
+}
+
+abstract class PruebaAbstracta{
+
+}
